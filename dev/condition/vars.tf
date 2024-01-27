70fd1e5c15609efbcfeceb204ef1fb0eb2a4f592
samanthpulluru <50269313+samanthpulluru@users.noreply.github.com>
2024-01-27 23:32:49 +0530

    Create vars.tf

---
+++ dev/condition/vars.tf
@@ -0,0 +1,12 @@
+variable "vpn_ip" {
+  default = "142.250.193.110/32"
+}
+
+variable "elb_names" {
+  type = list
+  default = ["dev-loadbalancer", "stage-loadbalancer", "prod-loadbalancer"]
+}
+
+variable "istest" {
+  default = "true"
+}
