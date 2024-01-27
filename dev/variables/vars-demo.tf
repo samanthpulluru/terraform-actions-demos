5cfdf32b4f08395c2dc8fe9e9940421a0ea69a88
samanthpulluru <50269313+samanthpulluru@users.noreply.github.com>
2024-01-27 20:08:58 +0530

    Create vars-demo.tf

---
+++ dev/variables/vars-demo.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region     = "us-west-2"
+}
+
+resource "aws_security_group" "vars_demo" {
+  name = "demo-variables"
+  
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["var.vpn_ip"]
+  }
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["var.vpn_ip"]
+  }
+
+ ingress {
+    from_port        = 53
+    to_port          = 53
+    protocol         = "tcp"
+    cidr_blocks      = ["var.vpn_ip"]
+  }
+
+}
