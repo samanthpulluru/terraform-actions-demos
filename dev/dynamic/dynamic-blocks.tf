b98d7b6b5dfbc8e87eb9467d8f8f4512bc4e26c6
samanthpulluru <50269313+samanthpulluru@users.noreply.github.com>
2024-01-31 22:27:56 +0530

    Create dynamic-blocks.tf

---
+++ dev/dynamic/dynamic-blocks.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "sg_port" {
+  type = list(number)
+  description = "List of ingress ports"
+  default = [8200, 8201, 8300]
+}
+
+resource "aws_security_group" "dynamic_sg" {
+  name = "dynamic-sg"
+  description = "Ingress for Vault"
+
+  dynamic "ingress" {
+    for_each = var.sg_port
+    content {
+      from_port = ingress.value
+      to_port = ingress.value
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
